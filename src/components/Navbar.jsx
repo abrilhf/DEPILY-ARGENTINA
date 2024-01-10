8311b64709bd0d617e285d66a8cb9bb984d40342
abrilhf <abril.hernandez@davinci.edu.ar>
2024-01-10 15:26:17 -0300

    TOCANDO CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 function NavBarDepily() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleNavCollapse = () => setExpanded(false);
+
   return (
-    <Navbar expand="lg" fixed="top" className="navBar">
+    <Navbar
+      expand="lg"
+      fixed="top"
+      className="navBar"
+      expanded={expanded}
+      onSelect={handleNavCollapse}
+    >
       <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link to="/">Home</Link>
